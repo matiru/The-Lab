00c75a4e9eac08a22238dcc45756383c4649b63f
matiru <gemmatiru@gmail.com>
2024-02-21 17:13:15 +0300

    Grid,Box and Anchor Layouts

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.anchorlayout  import AnchorLayout
+from kivy.uix.gridlayout  import GridLayout
+
+
+
+
+
+
+
+# class GridLayoutExample(GridLayout):
+#     pass
+class AnchorLayoutExample(AnchorLayout):
+    pass
+class BoxLayoutExample(BoxLayout):
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.orientation = 'vertical'
+    #     b1 = Button(text="A")
+    #     b2 = Button(text="B")
+    #     b3 = Button(text="B")
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    #     self.add_widget(b3)
+    pass
 
 
 class MainWidget(Widget):
+    # defines user interface
     pass
 
 
