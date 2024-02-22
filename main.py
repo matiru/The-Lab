9165707ad7e89c9be462b4b12e2241bc4ef61ca7
matiru <gemmatiru@gmail.com>
2024-02-22 15:13:07 +0300

    page,scroll,stack

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout  import AnchorLayout
 from kivy.uix.gridlayout  import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
 
 
 
 
 
 
+class StackLayoutExample(StackLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'lr-tb'
+        for i in range(0,100):
+            size = dp(100)
+            b = Button(text=str(i+1), size_hint=(None,None), size =(size,size))
+            self.add_widget(b)
 
-# class GridLayoutExample(GridLayout):
-#     pass
+
+    pass
 class AnchorLayoutExample(AnchorLayout):
     pass
 class BoxLayoutExample(BoxLayout):
