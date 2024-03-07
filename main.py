c098ea22fab5c33fa4743cf11dbb36848167a292
matiru <gemmatiru@gmail.com>
2024-03-07 16:33:23 +0300

    more_widgets

--- main.py
+++ main.py
@@ -6,9 +6,53 @@ from kivy.uix.anchorlayout  import AnchorLayout
 from kivy.uix.gridlayout  import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivy.properties import BooleanProperty
+class WidgetsExample(GridLayout):
+    my_text = StringProperty("Hello!")
+    count = 1
+    # state = StringProperty("OFF")
+    count_enabled = BooleanProperty(False)
+    #slider_value_txt = StringProperty('Value')
+
+
+    def on_button_click(self):
+        #if self.state == 'ON':
+        if self.count_enabled == True:
+            print('Button Clicked')
+            print(self.count)
+            self.count = self.count + 1
+            self.my_text = str(self.count)
+
+
+    def on_toggle_button_state(self,toggle_button):
+        print('Toggle State' + toggle_button.state )
+        if toggle_button.state == 'normal':
+            # self.state ='OFF'
+            toggle_button.text = 'off'
+            self.count_enabled = False
+        else:
+         # self.state='ON'
+             toggle_button.text = 'on'
+             self.count_enabled = True
+
+    def on_switch_active(self,widget):
+        print('Switch active' + str(widget.active))
+
+    def on_slider_value(self, widget):
+        print('Slider' + str(int(widget.value)))
+        #self.slider_value_txt =str(int(widget.value))
 
 
 
+
+
+
+
+
+
+
+    pass
 
 
 
